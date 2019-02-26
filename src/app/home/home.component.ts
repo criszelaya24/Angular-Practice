eec9fbaedceaa7db3bc3f09956deae0ac83aba61
PalaciosZ <criszelaya24@hotmail.com>
2019-02-26 11:12:11 +0100

    Adding classes and style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  h1Syle: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
   firstClick(){
-    alert("clicked!")
+    this.h1Syle = true;
   }
 }
